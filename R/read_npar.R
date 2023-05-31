3be9349ffc974d9fd802a4ee8cc90b2a73665a26
Arni Magnusson <thisisarni@gmail.com>
2023-05-31 15:15:23 +1100

    Link to read.MFCLPar() help page

--- R/read_npar.R
+++ R/read_npar.R
@@ -6,6 +6,10 @@
 #'
 #' @return Number of parameters as an integer.
 #'
+#' @seealso
+#' \code{\link[FLR4MFCL]{read.MFCLPar}} reads all the information contained in a
+#' \file{.par} file, which takes substantially longer.
+#'
 #' @examples
 #' \dontrun{
 #' read_npar("10.par")
