33f66d101c5975f85060729dcb3b16503c407962
Arni Magnusson <thisisarni@gmail.com>
2023-05-31 14:50:08 +1100

    Remove calls to help()

--- R/run_hessian.R
+++ R/run_hessian.R
@@ -1,12 +1,5 @@
 local.dir <- "c:/x/yft/hessian"
 
-library(help="TAF")
-help(package="TAF")
-help("TAF")
-help("TAF-package")
-?TAF
-?"TAF-package"
-
 run_hessian <- function(local.dir, ...)
 {
   condor_submit(local.dir=local.dir, ...)
