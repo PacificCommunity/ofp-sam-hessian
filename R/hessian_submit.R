7521f7b298df8a9b3c92eb0175fc543df6b97d97
Arni Magnusson <thisisarni@gmail.com>
2023-06-09 12:16:41 +1100

    Return job id vector

--- R/hessian_submit.R
+++ R/hessian_submit.R
@@ -7,8 +7,7 @@
 #'        Hessian computations.
 #' @param \dots passed to \code{\link{condor_submit}}.
 #'
-#' @return
-#' Type of output.
+#' @return Remote directory names with the job id as a name attribute.
 #'
 #' @examples
 #' \dontrun{
@@ -22,11 +21,22 @@
 
 hessian_submit <- function(working.dir, top.dir="condor_hessian", ...)
 {
+  # Examine directories
   dirs <- dir(working.dir, full.names=TRUE)
   dirs <- dirs[order(as.integer(gsub(".*_([0-9]+)", "\\1", dirs)))]  # sort
-  for(i in seq_along(dirs))
+  n <- length(dirs)
+
+  # Submit each model run directory to Condor, remembering job ids
+  jobs <- character(n)
+  ids <- integer(n)
+  for(i in seq_len(n))
   {
     cat("Submitting ", basename(dirs[i]), "\n", sep="")
-    condor_submit(local.dir=dirs[i], top.dir=top.dir, ...)
+    job <- condor_submit(local.dir=dirs[i], top.dir=top.dir, ...)
+    jobs[i] <- job
+    ids[i] <- as.integer(names(job))
   }
+  names(jobs) <- ids
+
+  jobs
 }
