c5fc4c3ff1b2907baf23f728c6017fc177be6e77
Arni Magnusson <thisisarni@gmail.com>
2023-07-24 18:36:23 +1100

    Rename hessian_stub() to hessian_standalone()

--- R/hessian_standalone.R
+++ R/hessian_standalone.R
@@ -1,7 +1,7 @@
-#' Hessian Stub
+#' Hessian Stand-Alone
 #'
-#' Prepare working directory for Hessian calculation, starting from a converged
-#' model run.
+#' Prepare working directory for Hessian stand-alone calculation, starting from
+#' a converged model run.
 #'
 #' @param original.dir directory containing a converged model run.
 #' @param working.dir directory where Hessian run will be prepared.
@@ -11,19 +11,19 @@
 #'
 #' @note
 
-#' The term \emph{stub} refers to a stand-alone calculation of the Hessian,
-#' starting from a converged model.
+#' The term \emph{stand-alone} refers to a calculation of the Hessian that
+#' starts from a converged model.
 #'
-#' A stub calculation takes a shorter time to run than a \emph{monolithic} run,
-#' where an extended \verb{doitall.sh} script first fits a model and then
-#' calculates the Hessian.
+#' A stand-alone calculation takes a shorter time to run than a
+#' \emph{monolithic} run, where an extended \verb{doitall.sh} script first fits
+#' a model and then calculates the Hessian.
 #'
-#' A stub calculation takes a longer time to run than a \emph{parallel} Hessian
-#' calculation, where computations are run simultaneously on many cores.
+#' A stand-alone calculation takes a longer time to run than a \emph{parallel}
+#' Hessian calculation, where computations are run simultaneously on many cores.
 #'
 #' @examples
 #' \dontrun{
-#' hessian_stub("z:/yft/2023/model_runs/diagnostic")
+#' hessian_standalone("z:/yft/2023/model_runs/diagnostic")
 #' }
 #'
 #' @importFrom FLR4MFCL finalPar
@@ -31,8 +31,8 @@
 #'
 #' @export
 
-hessian_stub <- function(original.dir, working.dir=basename(original.dir),
-                         overwrite=FALSE)
+hessian_standalone <- function(original.dir, working.dir=basename(original.dir),
+                               overwrite=FALSE)
 {
   # 1  List required files
   parfile <- basename(finalPar(original.dir))
@@ -53,19 +53,19 @@ hessian_stub <- function(original.dir, working.dir=basename(original.dir),
 
   # 3  Prepare scripts
   condor.run <- readLines(file.path(working.dir, "condor_run.sh"))
-  condor.run <- gsub("doitall", "dohessian_stub", condor.run)
-  dohessian.stub <- c("#!/bin/bash", "",
-                      paste("mfclo64", frqfile, parfile,
-                            "hessian -switch 2", "1 1 1", "1 145 1"),
-                      paste("mfclo64", frqfile, parfile,
-                            "hessian -switch 2", "1 1 1", "1 145 5"))
+  condor.run <- gsub("doitall", "dohessian_standalone", condor.run)
+  dohessian.standalone <- c("#!/bin/bash", "",
+                            paste("mfclo64", frqfile, parfile,
+                                  "hessian -switch 2", "1 1 1", "1 145 1"),
+                            paste("mfclo64", frqfile, parfile,
+                                  "hessian -switch 2", "1 1 1", "1 145 5"))
 
   # 4  Write scripts to files
   con <- file(file.path(working.dir, "condor_run.sh"), "wb")
-  writeLines(condor.run, con)      # must have Unix line endings
+  writeLines(condor.run, con)            # must have Unix line endings
   close(con)
-  con <- file(file.path(working.dir, "dohessian_stub.sh"), "wb")
-  writeLines(dohessian.stub, con)  # must have Unix line endings
+  con <- file(file.path(working.dir, "dohessian_standalone.sh"), "wb")
+  writeLines(dohessian.standalone, con)  # must have Unix line endings
   close(con)
 
   invisible(dir(working.dir))
