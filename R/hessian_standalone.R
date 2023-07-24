71c261baece6aec08305a535a1adf5a93813b643
Arni Magnusson <thisisarni@gmail.com>
2023-07-24 21:19:09 +1100

    Default working.dir = NULL

--- R/hessian_standalone.R
+++ R/hessian_standalone.R
@@ -7,10 +7,14 @@
 #' @param working.dir directory where Hessian run will be prepared.
 #' @param overwrite whether to remove existing working directory.
 #'
-#' @return Filenames in working directory after preparation.
+#' @details
+#' The default value of \code{working.dir = NULL} creates a local working
+#' directory with the same name as \code{original.dir} but with a prefix
+#' \verb{Hess_}.
+#'
+#' @return Working directory name where files have been prepared.
 #'
 #' @note
-
 #' The term \emph{stand-alone} refers to a calculation of the Hessian that
 #' starts from a converged model.
 #'
@@ -31,10 +35,16 @@
 #'
 #' @export
 
-hessian_standalone <- function(original.dir, working.dir=basename(original.dir),
-                               overwrite=FALSE)
+hessian_standalone <- function(original.dir, working.dir=NULL, overwrite=FALSE,
+                               quiet=FALSE)
 {
-  # 1  List required files
+  # 1  Create working directory
+  if(is.null(working.dir))
+    working.dir <- paste0("Hess_", basename(original.dir))
+  if(!quiet)
+    cat("Preparing", basename(working.dir), "... ")
+
+  # 2  List required files
   parfile <- basename(finalPar(original.dir))
   species <- file_path_sans_ext(grep("\\.frq$", dir(original.dir), value=TRUE))
   agelenfile <- paste0(species, ".age_length")
@@ -43,7 +53,7 @@ hessian_standalone <- function(original.dir, working.dir=basename(original.dir),
   files <- c(parfile, "condor.sub", "condor_run.sh", "doitall.sh", "labels.tmp",
              "mfcl.cfg", "mfclo64", agelenfile, frqfile, tagfile)
 
-  # 2  Copy required files to working directory
+  # 3  Copy required files to working directory
   if(dir.exists(working.dir) && !overwrite)
     stop("working.dir already exists, consider overwrite=TRUE")
   unlink(working.dir, recursive=TRUE)
@@ -51,7 +61,7 @@ hessian_standalone <- function(original.dir, working.dir=basename(original.dir),
   suppressWarnings(file.copy(file.path(original.dir, files), working.dir,
                              copy.date=TRUE))  # some files could be missing
 
-  # 3  Prepare scripts
+  # 4  Prepare scripts
   condor.run <- readLines(file.path(working.dir, "condor_run.sh"))
   condor.run <- gsub("doitall", "dohessian_standalone", condor.run)
   dohessian.standalone <- c("#!/bin/bash", "",
@@ -60,13 +70,15 @@ hessian_standalone <- function(original.dir, working.dir=basename(original.dir),
                             paste("mfclo64", frqfile, parfile,
                                   "hessian -switch 2", "1 1 1", "1 145 5"))
 
-  # 4  Write scripts to files
+  # 5  Write scripts to files
   con <- file(file.path(working.dir, "condor_run.sh"), "wb")
   writeLines(condor.run, con)            # must have Unix line endings
   close(con)
   con <- file(file.path(working.dir, "dohessian_standalone.sh"), "wb")
   writeLines(dohessian.standalone, con)  # must have Unix line endings
   close(con)
+  if(!quiet)
+    cat("done", fill=TRUE)
 
-  invisible(dir(working.dir))
+  invisible(working.dir)
 }
