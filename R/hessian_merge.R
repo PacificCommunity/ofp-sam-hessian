8eb6d07c5b011b6d94dc1f23725f68894192724f
Arni Magnusson <thisisarni@gmail.com>
2023-07-24 13:50:21 +1100

    Implement hessian_merge()

--- R/hessian_merge.R
+++ R/hessian_merge.R
@@ -3,40 +3,61 @@
 #' Gather \verb{.hes} files from parallel computations into one directory.
 #'
 #' @param working.dir local directory containing Hessian subdirectories.
-#' @param top.dir top directory on Condor submitter machine containing parallel
-#'        Hessian computations.
-#' @param \dots passed to \code{\link{condor_download}}.
+#' @param quiet whether to suppress messages.
 #'
-#' @return Data frame showing downloaded jobs.
+#' @return Directory containing the \verb{.hes} files, ready to merge.
 #'
 #' @examples
 #' \dontrun{
-#' session <- ssh_connect("servername")
-#' hessian_merge("c:/yft/hessian")
+#' hessian_merge("z:/yft/2023/model_runs/hessian/diagnostic")
 #' }
 #'
-#' @importFrom condor condor_dir condor_download
+#' @importFrom tools file_path_sans_ext
+#' @importFrom FLR4MFCL finalPar
 #'
 #' @export
 
-hessian_merge <- function(working.dir=".", top.dir="condor_hessian", ...)
+hessian_merge <- function(model, quiet=FALSE)
 {
-  jobs <- condor_dir(top.dir=top.dir)
+  if(!quiet)
+    cat("*", basename(model), fill=TRUE)
 
-  # Ensure all jobs are finished
-  if(any(jobs$status != "finished"))
+  # 1  Create merge directory
+  merge.dir <- file.path(model, "merge")
+  unlink(merge.dir, recursive=TRUE)
+  dir.create(merge.dir)
+
+  # 2  Copy .hes files from stripe dirs into model dir
+  from <- dir(model, pattern="\\.hes$", full.names=TRUE, recursive=TRUE)
+  to <- file.path(merge.dir, paste0(basename(from), "_", seq_along(from)))
+  for(i in seq_along(from))
   {
-    print(jobs)
-    stop("all jobs must have status 'finished'")
+    if(!quiet)
+      cat(" ", basename(to)[i], fill=TRUE)
+    file.copy(from[i], to[i], overwrite=TRUE, copy.date=TRUE)
   }
 
-  for(i in seq_len(nrow(jobs)))
-  {
-    cat("* Gathering ", jobs$dir[i], "\n", sep="")
-    condor_download(run.dir=jobs$dir[i],
-                    local.dir=file.path(working.dir, jobs$dir[i]),
-                    top.dir=top.dir, create.dir=TRUE, overwrite=TRUE)
-  }
+  # 3  Copy additional required model files from 1st run directory
+  species <- file_path_sans_ext(basename(from))[1]
+  species.files <- paste0(species, ".", c("age_length", "frq", "tag"))
+  first.dir <- dirname(from)[1]
+  parfile <- basename(finalPar(first.dir))
+  files <- file.path(first, c("mfcl.cfg", "mfclo64", "parall_hess",
+                              species.files, parfile))
+  file.copy(files, merge.dir, overwrite=TRUE, copy.date=TRUE)
 
-  invisible(jobs)
+  # 4  Create Bash scripts
+  dohessian.merge <- paste0("mfclo64 ", species, ".frq ", parfile,
+                            " ccc -switch 1 1 145 11")
+  dohessian.report <- paste0("mfclo64 ", species, ".frq ", parfile,
+                             " ccc -switch 1 1 145 5")
+  # Bash scripts must have Unix line endings
+  con <- file(file.path(merge.dir, "dohessian_merge.sh"), "wb")
+  writeLines(dohessian.merge, con)
+  close(con)
+  con <- file(file.path(merge.dir, "dohessian_report.sh"), "wb")
+  writeLines(dohessian.report, con)
+  close(con)
+
+  invisible(merge.dir)
 }
