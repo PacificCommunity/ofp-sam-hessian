252cf7873dcc9bc78a937610e980866400c8cb3e
Arni Magnusson <thisisarni@gmail.com>
2023-07-24 18:30:38 +1100

    Rename hessian_serial() to hessian_stub()

--- R/hessian_stub.R
+++ R/hessian_stub.R
@@ -1,6 +1,7 @@
-#' Hessian Serial
+#' Hessian Stub
 #'
-#' Prepare working directory for serial Hessian calculation.
+#' Prepare working directory for Hessian calculation, starting from a converged
+#' model run.
 #'
 #' @param original.dir directory containing a converged model run.
 #' @param working.dir directory where Hessian run will be prepared.
@@ -8,9 +9,21 @@
 #'
 #' @return Filenames in working directory after preparation.
 #'
+#' @note
+
+#' The term \emph{stub} refers to a stand-alone calculation of the Hessian,
+#' starting from a converged model.
+#'
+#' A stub calculation takes a shorter time to run than a \emph{monolithic} run,
+#' where an extended \verb{doitall.sh} script first fits a model and then
+#' calculates the Hessian.
+#'
+#' A stub calculation takes a longer time to run than a \emph{parallel} Hessian
+#' calculation, where computations are run simultaneously on many cores.
+#'
 #' @examples
 #' \dontrun{
-#' hessian_serial("z:/yft/2023/model_runs/diagnostic")
+#' hessian_stub("z:/yft/2023/model_runs/diagnostic")
 #' }
 #'
 #' @importFrom FLR4MFCL finalPar
@@ -18,8 +31,8 @@
 #'
 #' @export
 
-hessian_serial <- function(original.dir, working.dir=basename(original.dir),
-                           overwrite=FALSE)
+hessian_stub <- function(original.dir, working.dir=basename(original.dir),
+                         overwrite=FALSE)
 {
   # 1  List required files
   parfile <- basename(finalPar(original.dir))
@@ -40,19 +53,19 @@ hessian_serial <- function(original.dir, working.dir=basename(original.dir),
 
   # 3  Prepare scripts
   condor.run <- readLines(file.path(working.dir, "condor_run.sh"))
-  condor.run <- gsub("doitall", "dohessian_serial", condor.run)
-  dohessian.serial <- c("#!/bin/bash", "",
-                        paste("mfclo64", frqfile, parfile,
-                              "hessian -switch 2", "1 1 1", "1 145 1"),
-                        paste("mfclo64", frqfile, parfile,
-                              "hessian -switch 2", "1 1 1", "1 145 5"))
+  condor.run <- gsub("doitall", "dohessian_stub", condor.run)
+  dohessian.stub <- c("#!/bin/bash", "",
+                      paste("mfclo64", frqfile, parfile,
+                            "hessian -switch 2", "1 1 1", "1 145 1"),
+                      paste("mfclo64", frqfile, parfile,
+                            "hessian -switch 2", "1 1 1", "1 145 5"))
 
   # 4  Write scripts to files
   con <- file(file.path(working.dir, "condor_run.sh"), "wb")
-  writeLines(condor.run, con)        # must have Unix line endings
+  writeLines(condor.run, con)      # must have Unix line endings
   close(con)
-  con <- file(file.path(working.dir, "dohessian_serial.sh"), "wb")
-  writeLines(dohessian.serial, con)  # must have Unix line endings
+  con <- file(file.path(working.dir, "dohessian_stub.sh"), "wb")
+  writeLines(dohessian.stub, con)  # must have Unix line endings
   close(con)
 
   invisible(dir(working.dir))
